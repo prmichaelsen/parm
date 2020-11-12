49b11a2b403633f56ffd6aaeea8ed377babe74e0
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2020-11-12 14:19:34 -0700

    chore: update backup script

--- tools/scripts/f5/backup.ts
+++ tools/scripts/f5/backup.ts
@@ -7,15 +7,13 @@ import {
 // run this script by entering this into the terminal: 
 // ./tools/f5/backup.ts
 const firestoreService = require('firestore-export-import');
-// this line will throw an error, but this is a backup
-// script one-off, so it's not worth it to me to
-// fix it.
-import { firebase } from  '../../../libs/util/env/secrets/firebase';
+
+const databaseUrl = 'https://parm-names-not-numbers.firebaseio.com';
 
 const serviceAccount = require('../../../env/parm-names-not-numbers.json');
  
 // Initiate Firebase App
-firestoreService.initializeApp(serviceAccount, firebase.databaseURL);
+firestoreService.initializeApp(serviceAccount, databaseUrl);
  
 // Start exporting your data
 firestoreService
