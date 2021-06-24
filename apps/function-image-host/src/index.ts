8a2dbfd81c51794be28eef4ad0b5f5c7ee9f429a
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2021-06-24 09:31:35 -0700

    bugfix: 404 if no image path at all

--- apps/function-image-host/src/index.ts
+++ apps/function-image-host/src/index.ts
@@ -13,6 +13,7 @@ admin.initializeApp({
 
 export const functionImageHost = async (req: any, res?: any) => {
 
+  // break out url to get the image-path.jpg portion
   const fp = req.url.split('/');
   const end = fp[fp.length - 1];
   const name = end.split('.')[0];
@@ -22,16 +23,10 @@ export const functionImageHost = async (req: any, res?: any) => {
   const filePath = `${ImagesStore}/${name}`;
   const bucketName = 'parm-names-not-numbers.appspot.com';
   const bucket = admin.storage().bucket(bucketName);
-  const fetchImage = async () => {
-    const image = bucket.file(filePath);
-    if (!image)
-      throw new Error();
-    return null;
-  }
-  const image = await fetchImage();
-  if (image === null) {
+  const image = bucket.file(filePath);
+  const [exists] = await image.exists();
+  if (!exists)
     return res.sendStatus(404);
-  }
   await image.makePublic();
   const r = image.createReadStream();
   const ps = new stream.PassThrough() // <---- this makes a trick with stream error handling
