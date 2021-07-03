58ef762485b3f11134730a86a69b02af0608efbd
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2021-07-02 20:07:21 -0700

    temp: expo wip

---
+++ apps/f5/expo/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'f5-expo',
+  preset: '../../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../../coverage/apps/f5/expo'
+};
